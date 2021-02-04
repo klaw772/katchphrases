75ebc2ffd49103e89a239bfc6e1ef153041a46d9
Kat Law <kathleenmlaw1@gmail.com>
2021-02-03 21:51:34 -0500

    removing flaky test, no need to test react bootstrap's already implemented behavior

--- src/__tests__/SoundboardContainer/SoundboardContainer.spec.js
+++ src/__tests__/SoundboardContainer/SoundboardContainer.spec.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
-import wait from "waait";
+import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
 import { SoundboardContainer } from "../../components/SoundboardContainer/SoundboardContainer";
@@ -9,14 +8,5 @@ describe("SoundboardContainer", () => {
   it("renders", () => {
     const { getByTestId } = render(<SoundboardContainer />);
     expect(getByTestId("soundboard-container")).toBeInTheDocument();
-  });
-  it("closes modal when dismissed", async () => {
-    const { getByTestId, getByText, queryByText } = render(
-      <SoundboardContainer />
-    );
-    expect(getByText("Welcome to Katchphrases!")).toBeInTheDocument();
-    await fireEvent.click(getByTestId("modal-close-button"));
-    await wait(0);
-    expect(queryByText("Welcome to Katchphrases!")).not.toBeInTheDocument();
   });
 });
