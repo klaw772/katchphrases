5606112bd92b73f56adfe36ed65e11581fc51e79
Kat Law <kathleenmlaw1@gmail.com>
2021-02-05 20:40:22 -0500

    adding sound assets to directory

--- src/components/SoundboardButtonsContainer/SoundboardButtonsContainer.js
+++ src/components/SoundboardButtonsContainer/SoundboardButtonsContainer.js
@@ -1,11 +1,8 @@
-import React, { useContext } from "react";
-import { ThemeContext } from "../ThemeContext/ThemeContext";
+import React from "react";
 
 export const SoundboardButtonsContainer = () => {
-  const theme = useContext(ThemeContext);
   return (
     <div>
-      <p>{theme}</p>
       <p>placeholder container to hold soundboard buttons</p>
     </div>
   );
